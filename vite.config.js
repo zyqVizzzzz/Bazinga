ecf73014599c779a572d5e3673ad70ca955858f0
zyqVizzzzz <liangsuzhi@gmail.com>
2025-03-24 17:11:23 +0800

    update: 修改代理, 把请求转到cloudflare work

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			"/api": {
-				target: "http://localhost:3000",
+				target: "https://bazinga.haloworm.workers.dev/",
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, ""),
 			},
